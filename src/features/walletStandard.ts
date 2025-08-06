474f9322fcf77b4435f29d15880e5956f24b62ad
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2025-08-06 20:12:06 +0800

    feat: messaging API and removed iota stuffs

--- src/features/walletStandard.ts
+++ src/features/walletStandard.ts
@@ -1,144 +1,130 @@
-import { type Wallet, SUPPORTED_CHAINS, ReadonlyWalletAccount  } from "@iota/wallet-standard";
-import { SUI_CHAINS } from "@mysten/wallet-standard";
-import {icon} from "./icon"; 
+import {
+  ReadonlyWalletAccount,
+  SUPPORTED_CHAINS,
+  type Wallet
+} from "@iota/wallet-standard"
+import { SUI_CHAINS } from "@mysten/wallet-standard"
 import type {
-    StandardConnectFeature,
-    StandardConnectMethod,
-    StandardEventsFeature,
-    StandardEventsOnMethod,
-    IotaFeatures,
-    IotaSignPersonalMessageMethod,
-    IotaSignTransactionMethod,
-    IotaSignAndExecuteTransactionMethod,
-    IotaReportTransactionEffectsMethod,
-    StandardConnectOutput,
-    IotaSignPersonalMessageOutput,
-    SignedTransaction,
-    IotaSignAndExecuteTransactionOutput
-} from "@iota/wallet-standard";
-import type { SuiFeatures } from "@mysten/wallet-standard";
-import { ETHEREUM_CHAINS } from "@wallet-standard/ethereum";
-import { getAllAccounts } from "~store/store";
+  StandardConnectFeature,
+  StandardConnectMethod,
+  StandardEventsFeature,
+  StandardEventsOnMethod,
+  SuiFeatures,
+  SuiReportTransactionEffectsMethod,
+  SuiSignAndExecuteTransactionMethod,
+  SuiSignPersonalMessageMethod,
+  SuiSignTransactionMethod
+} from "@mysten/wallet-standard"
+import { ETHEREUM_CHAINS } from "@wallet-standard/ethereum"
+
+import { getAllAccounts } from "~store/store"
+
+import { icon } from "./icon"
 
 export class SeaWallet implements Wallet {
-    get version(): "1.0.0" {
-        // Return the version of the Wallet Standard this implements (in this case, 1.0.0).
-        return '1.0.0';
+  get version(): "1.0.0" {
+    // Return the version of the Wallet Standard this implements (in this case, 1.0.0).
+    return "1.0.0"
+  }
+  get name() {
+    return "Sea Wallet "
+  }
+  get icon(): `data:image/png;base64,${string}` {
+    // A simple wallet icon in SVG format, base64 encoded
+    return icon
+  }
+  // Return the Sui chains that your wallet supports.
+  get chains() {
+    return SUPPORTED_CHAINS.concat(SUI_CHAINS, ETHEREUM_CHAINS)
+  }
+  private walletAccounts: ReadonlyWalletAccount[] = []
+
+  constructor() {
+    // Initialize accounts
+    this.refreshAccounts()
+  }
+
+  private async refreshAccounts() {
+    const accounts = await getAllAccounts()
+    this.walletAccounts = accounts.map(
+      (walletAccount) =>
+        new ReadonlyWalletAccount({
+          address: walletAccount.address,
+          publicKey: walletAccount.keypair.getPublicKey().toString(),
+          // The Sui chains that your wallet supports.
+          chains: SUI_CHAINS,
+          // The features that this account supports. This can be a subset of the wallet's supported features.
+          // These features must exist on the wallet as well.
+          features: [
+            "sui:signPersonalMessage",
+            "sui:signTransaction",
+            "sui:signAndExecuteTransaction"
+          ]
+        })
+    )
+  }
+
+  get accounts() {
+    // Return the cached accounts that your wallet has
+    return this.walletAccounts
+  }
+  get features(): StandardConnectFeature & StandardEventsFeature & SuiFeatures {
+    return {
+      "standard:connect": {
+        version: "1.0.0",
+        connect: this.#connect
+      },
+      "standard:events": {
+        version: "1.0.0",
+        on: this.#on
+      },
+
+      "sui:signPersonalMessage": {
+        version: "1.1.0",
+        signPersonalMessage: this.#signPersonalMessage
+      },
+      "sui:signTransaction": {
+        version: "2.0.0",
+        signTransaction: this.#signTransaction
+      },
+      "sui:signAndExecuteTransaction": {
+        version: "2.0.0",
+        signAndExecuteTransaction: this.#signAndExecuteTransaction
+      },
+      "sui:reportTransactionEffects": {
+        version: "1.0.0",
+        reportTransactionEffects: this.#reportTransactionEffects
+      }
     }
-    get name() {
-        return 'Sea Wallet ';
-    }
-    get icon(): `data:image/png;base64,${string}` {
-        // A simple wallet icon in SVG format, base64 encoded
-        return icon ;
-    }
-    // Return the Sui chains that your wallet supports.
-    get chains() {
-        return SUPPORTED_CHAINS.concat(SUI_CHAINS, ETHEREUM_CHAINS);
-    }
-    private walletAccounts: ReadonlyWalletAccount[] = [];
+  }
 
-    constructor() {
-        // Initialize accounts
-        this.refreshAccounts();
-    }
+  #on: StandardEventsOnMethod = () => {
+    // Your wallet's events on implementation.
+    return () => {}
+  }
 
-    private async refreshAccounts() {
-        const accounts = await getAllAccounts();
-        this.walletAccounts = accounts.map(
-            (walletAccount) =>
-                new ReadonlyWalletAccount({
-                    address: walletAccount.address,
-                    publicKey: walletAccount.keypair.getPublicKey().toString(),
-                    // The Sui chains that your wallet supports.
-                    chains: SUI_CHAINS,
-                    // The features that this account supports. This can be a subset of the wallet's supported features.
-                    // These features must exist on the wallet as well.
-                    features: [
-                        'sui:signPersonalMessage',
-                        'sui:signTransaction',
-                        'sui:signAndExecuteTransaction',
-                    ],
-                }),
-        );
-    }
+  #connect: StandardConnectMethod = async () => {
+    // Your wallet's connect implementation
+    return { accounts: [] }
+  }
 
-    get accounts() {
-        // Return the cached accounts that your wallet has
-        return this.walletAccounts;
-    }
-    get features(): StandardConnectFeature & StandardEventsFeature & IotaFeatures & SuiFeatures {
-        return {
-            "standard:connect": {
-                version: "1.0.0",
-                connect: this.#connect,
-            },
-            "standard:events": {
-                version: "1.0.0",
-                on: this.#on,
-            },
-            "iota:signPersonalMessage": {
-                version: "1.0.0",
-                signPersonalMessage: this.#signPersonalMessage,
-            },
-            "iota:signTransaction": {
-                version: "2.0.0",
-                signTransaction: this.#signTransaction,
-            },
-            "iota:signAndExecuteTransaction": {
-                version: "2.0.0",
-                signAndExecuteTransaction: this.#signAndExecuteTransaction,
-            },
-            "iota:reportTransactionEffects": {
-                version: "1.0.0",
-                reportTransactionEffects: this.#reportTransactionEffects,
-            },
-            "sui:signPersonalMessage": {
-                version: "1.1.0",
-                signPersonalMessage: this.#signPersonalMessage,
-            },
-            "sui:signTransaction": {
-                version: "2.0.0",
-                signTransaction: this.#signTransaction,
-            },
-            "sui:signAndExecuteTransaction": {
-                version: "2.0.0",
-                signAndExecuteTransaction: this.#signAndExecuteTransaction,
-            },
-            "sui:reportTransactionEffects": {
-                version: "1.0.0",
-                reportTransactionEffects: this.#reportTransactionEffects,
-            },
-        };
-    }
+  #signPersonalMessage: SuiSignPersonalMessageMethod = async () => {
+    // Your wallet's signPersonalMessage implementation
+    throw new Error("Not implemented")
+  }
 
-    #on: StandardEventsOnMethod = () => {
-        // Your wallet's events on implementation.
-        return () => {};
-    };
+  #signTransaction: SuiSignTransactionMethod = async () => {
+    // Your wallet's signTransaction implementation
+    throw new Error("Not implemented")
+  }
 
-    #connect: StandardConnectMethod = async () => {
-        // Your wallet's connect implementation
-        return { accounts: [] };
-    };
+  #signAndExecuteTransaction: SuiSignAndExecuteTransactionMethod = async () => {
+    // Your wallet's signAndExecuteTransaction implementation
+    throw new Error("Not implemented")
+  }
 
-    #signPersonalMessage: IotaSignPersonalMessageMethod = async () => {
-        // Your wallet's signPersonalMessage implementation
-        throw new Error("Not implemented");
-    };
-
-    #signTransaction: IotaSignTransactionMethod = async () => {
-        // Your wallet's signTransaction implementation
-        throw new Error("Not implemented");
-    };
-
-    #signAndExecuteTransaction: IotaSignAndExecuteTransactionMethod = async () => {
-        // Your wallet's signAndExecuteTransaction implementation
-        throw new Error("Not implemented");
-    };
-
-    #reportTransactionEffects: IotaReportTransactionEffectsMethod = async () => {
-        // Your wallet's reportTransactionEffects implementation
-        throw new Error("Not implemented");
-    };
+  #reportTransactionEffects: SuiReportTransactionEffectsMethod = async () => {
+    // Your wallet's reportTransactionEffects implementation
+    throw new Error("Not implemented")
+  }
 }
