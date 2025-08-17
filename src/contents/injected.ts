3df05255f55cbb27b118972c638633b2fab20fda
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2025-08-17 12:59:39 +0800

    directory change

---
+++ src/contents/injected.ts
@@ -0,0 +1,34 @@
+import { registerWallet } from "@mysten/wallet-standard"
+import type { PlasmoCSConfig } from "plasmo"
+
+import { sendToBackground } from "@plasmohq/messaging"
+
+import { SeaWallet } from "../features/walletStandard"
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"],
+  world: "MAIN"
+}
+
+registerWallet(new SeaWallet())
+console.log("Hello Main World")
+
+// Wrap async code in an async function and execute it immediately
+async function connectToBackground() {
+  let res
+  console.log("Getting extension ID")
+  const extensionId = chrome.runtime.id
+  console.log("Extension ID:", extensionId)
+  res = await sendToBackground({
+    name: "connect",
+    body: {
+      site: window.location.origin,
+      icon: `${window.location.origin}/favicon.ico`
+    },
+    extensionId: 'anaeleemhdicpgmclmdijcmadhmeipfp'
+  })
+  console.log("Response from background:", res)
+}
+
+// Call the async function
+connectToBackground()
